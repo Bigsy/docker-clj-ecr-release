6383474b34e670893a3855fe6585c05a5e57f7aa
Bill Hedworth <bill.hedworth@ovoenergy.com>
2016-10-10 19:02:17 +0100

    stuff

--- project.clj
+++ project.clj
@@ -70,7 +70,6 @@
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag --no-sign"]
-                  ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]]
